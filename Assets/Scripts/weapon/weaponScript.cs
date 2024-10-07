afdea0bd55c588781fb7bb876ff5a713b633c2a9
João Gabriel Leal <134751489+SrLeal42@users.noreply.github.com>
2024-10-07 15:09:53 -0300

    Update weaponScript.cs

--- Assets/Scripts/weapon/weaponScript.cs
+++ Assets/Scripts/weapon/weaponScript.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class weaponScript : MonoBehaviour
 {
+    [Header("Weapons")]
     public weaponData WData;
     public weaponData[] allWeapons;
     private int WIndex = 0;
-    [HideInInspector] public bool shooting = false;
+
+    [Header("Tiro")]
+    //[HideInInspector] public bool shooting = false;
     private float timeSinceLastShoot;
     public Transform muzzle;
     public Camera playerCamera;
     private Transform weaponTransform;
-    private bool CanShoot => !WData.reloading && WData.currentAmmo > 0 && !shooting;
+
+    [Header("UI")]
+    public TMP_Text weaponText;
+    public Image weaponTextBackGround;
+    public TMP_Text weaponReloadText;
+    public Image weaponReloadTextBackGround;
+
+    private bool CanShoot => !WData.reloading && WData.currentAmmo > 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
         weaponTransform = GetComponent<Transform>();
+        adjustingWeaponNameText(WData.name);
     }
 
     // Update is called once per frame
@@ -30,11 +43,15 @@ public class weaponScript : MonoBehaviour
             changeWeapon();
         }
 
-        timeSinceLastShoot += Time.deltaTime;
 
-        if (timeSinceLastShoot >= WData.shootingTime)
+        toggleReloadText(WData.reloading);
+
+        timeSinceLastShoot -= Time.deltaTime;
+
+        if (timeSinceLastShoot <= 0)
         {
-            shooting = false;
+            WData.reloading = false;
+            //shooting = false;
         }
 
         Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
@@ -49,8 +66,7 @@ public class weaponScript : MonoBehaviour
     {
         if (CanShoot)
         {
-            shooting = true;
-            timeSinceLastShoot = 0;
+            //shooting = true;
 
             Transform bulletObj = Instantiate(WData.bulletPrefab, muzzle.position, muzzle.rotation);
             bulletObj.GetComponent<projectile>().vel = power;
@@ -58,7 +74,8 @@ public class weaponScript : MonoBehaviour
             bulletObj.transform.forward = _dir;
 
             //WData.currentAmmo--;
-            timeSinceLastShoot = 0f;
+            WData.reloading = true;
+            timeSinceLastShoot = WData.reloadingTime;
         }
 
     }
@@ -77,7 +94,28 @@ public class weaponScript : MonoBehaviour
 
         WData = allWeapons[WIndex];
 
+        adjustingWeaponNameText(WData.name);
+
     }
 
+
+    public void adjustingWeaponNameText(string weaponName, int gap = 30)
+    {
+        weaponText.text = weaponName;
+        weaponTextBackGround.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, weaponText.preferredWidth + gap);
+    }
+
+    public void adjustingWeaponReloadText(string time, int gap = 50)
+    {
+        weaponReloadText.text = time;
+        weaponReloadTextBackGround.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, weaponReloadText.preferredWidth + gap);
+    }
+
+    public void toggleReloadText(bool enable)
+    {
+        weaponReloadText.enabled = enable;
+        weaponReloadTextBackGround.enabled = enable;
+        adjustingWeaponReloadText(timeSinceLastShoot.ToString("F2"));
+    }
 
 }
