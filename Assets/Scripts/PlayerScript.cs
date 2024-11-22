0ba788a47b11018e3860d98d703d7c3473592b25
João Gabriel Leal <134751489+SrLeal42@users.noreply.github.com>
2024-11-21 21:15:26 -0300

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,11 +12,22 @@ public class PlayerScript : MonoBehaviour
 
     public CSVScript CSVScript;
 
+    // PROVISORIO
+    public PythonConnection PC;
+    public TMP_Text precisao;
+    public TMP_Text distancia;
+    public TMP_Text acertosPorSegundo;
+    //PROVISORIO
+
     public weaponScript currentWeapon;
     public float power = 0f;
     public float maxPower = 50f;
     public Transform muzzle;
     public Camera playerCamera;
+
+    public SpawnerScript spawner;
+
+    [HideInInspector] public float tempo_jogo = 0;
 
     [Header("Game State")]
     public string gameState = "menuinicial";
@@ -24,6 +36,9 @@ public class PlayerScript : MonoBehaviour
     Dictionary<string, Canvas> CanvasDic = new Dictionary<string, Canvas>();
     public Canvas canvasHUD;
     public Canvas canvasMenu;
+    public Canvas canvasGameOver;
+    public Canvas canvasEstatistica;
+    public Canvas canvasOpcoes;
 
     [Header("Gravidade")]
     public float gravEstatica = -9.81f; // Gravidade estatica 
@@ -37,10 +52,16 @@ public class PlayerScript : MonoBehaviour
     [Header("Timer")]
     float maxTempo = 120f;
     float currentTime = 120f;
-    public float multTimer = 1f;
+
+    private float multTimerInit = 1f; // Valor inicial para o mutiplicador do timer
+    public float multTimer;
     float multTimerMax = 15f; // Limite que multTimer pode chegar 
     float DecreaseAmountTimer = 0.05f; // Usamos essa variavel para aumentar o multTimer com o tempo
     float IncreaseAmountTimer = 0.2f; // Usamos essa variavel para multiplicar o multTimer e reduzir a quantidade de tempo aumentando ao estourar um balao
+
+    [Header("Opçoes")]
+    public TMP_InputField inputEnvironment;
+
     public Slider timerBar;
     public Image fillImage;
     private Color fullColor = Color.green; // Cor da barra cheia
@@ -52,9 +73,14 @@ public class PlayerScript : MonoBehaviour
 
         CanvasDic.Add("HUD", canvasHUD);
         CanvasDic.Add("Menu", canvasMenu);
+        CanvasDic.Add("GameOver", canvasGameOver);
+        CanvasDic.Add("Estatistica", canvasEstatistica);
+        CanvasDic.Add("Opcoes", canvasOpcoes);
 
         // Setando o tempo atual no maximo
         currentTime = maxTempo;
+
+        multTimer = multTimerInit;
 
         // Alterando o alpha da linha
         Color startColor = lineRenderer.startColor;
@@ -78,7 +104,19 @@ public class PlayerScript : MonoBehaviour
             case "menuinicial":
                 if (canvasMenu.enabled != true) changeCanvas("Menu");
                 break;
-        
+
+            case "gameover":
+                if (canvasGameOver.enabled != true) changeCanvas("GameOver");
+                break;
+            
+            case "estatistica":
+                if (canvasEstatistica.enabled != true) changeCanvas("Estatistica");
+                break;
+
+            case "opcoes":
+                if (canvasOpcoes.enabled != true) changeCanvas("Opcoes");
+                break;
+
         }
 
 
@@ -89,6 +127,9 @@ public class PlayerScript : MonoBehaviour
     {
         if(canvasHUD.enabled != true) changeCanvas("HUD");
 
+        tempo_jogo += Time.deltaTime;
+        //Debug.Log(tempo_jogo);
+        
         decreasingTimeAndUpdateBar();
 
         if (Input.GetKey(KeyCode.Mouse0))
@@ -150,7 +191,12 @@ public class PlayerScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            CSVScript.saving();
+            changeGameState("gameover");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGame();
         }
     }
 
@@ -208,6 +254,11 @@ public class PlayerScript : MonoBehaviour
         timerBar.value = currentTime / maxTempo; // Atualizando a barra de progresso com o valor do tempo atual 
         
         fillImage.color = Color.Lerp(emptyColor, fullColor, timerBar.value); // Modificando a cor da barra para efeito visual
+
+        if (currentTime <= 0)
+        {
+            changeGameState("gameover");
+        }
     }
 
     public void increasingTimeAndUpdateBar(float increase)
@@ -219,5 +270,70 @@ public class PlayerScript : MonoBehaviour
         fillImage.color = Color.Lerp(emptyColor, fullColor, timerBar.value); // Modificando a cor da barra para efeito visual
     }
 
+
+    public void ResetParamter()
+    {
+        multTimer = multTimerInit; // Resetando o mutiplicador do tempo
+
+        currentTime = maxTempo; // Resetando o tempo do timer
+
+        timerBar.value = currentTime / maxTempo; // Resetando a barra de progresso com o valor do máximo 
+        fillImage.color = fullColor; // Resetando a cor da barra
+
+        tempo_jogo = 0; // Resetando o tempo de jogo
+    }
+
+    public void DestroyAllBallons()
+    {
+        BallonScript[] ballons = FindObjectsOfType<BallonScript>();
+
+        foreach (BallonScript b in ballons)
+        {
+            Destroy(b.gameObject);
+        }
+    }
+
+    public void ResetGame()
+    {
+        ResetParamter();
+        spawner.ResetParamter();
+        CSVScript.ResetLinhas();
+        DestroyAllBallons();
+        changeGameState("playing");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("O jogo está sendo encerrado."); // Apenas para testes no Editor
+        Application.Quit();
+    }
+
+    public void Estatisticas()
+    {
+        CSVScript.saving(escolherDiretorio: false);
+        string result = PC.PythonCalcularDesempenho();
+
+        UnityEngine.Debug.Log(result);
+
+        string[] resultSplit = result.Split(' ');
+
+        precisao.text = resultSplit[0];
+        distancia.text = resultSplit[1];
+        acertosPorSegundo.text = resultSplit[2];
+
+        changeGameState("estatistica");
+    }
+
+
+    public void saveOpcoes()
+    {
+        //PC.PathEnvironment = inputEnvironment.text.Trim();
+        string PATH = inputEnvironment.text.Trim();
+        string[] PATHsplit = PATH.Split("\\");
+        PATH += PATHsplit[PATHsplit.Length - 1] != "python.exe" ? "\\python.exe" : "";
+        PC.PathEnvironment = PATH;
+
+        Debug.Log(PC.PathEnvironment);
+    }
 
 }
