d2bbb66794d1a073e2b0fd5a206b3efe16c4267c
João Gabriel Leal <134751489+SrLeal42@users.noreply.github.com>
2024-11-21 21:14:47 -0300

    Update SaveCSV.cs

--- Assets/Scripts/SaveCSV.cs
+++ Assets/Scripts/SaveCSV.cs
@@ -7,22 +7,41 @@ using System.IO;
 
 public class SaveCSV : MonoBehaviour
 {
-    public static void saveCSV(string linhas)
+
+    public static string diretorio_padrao = Directory.GetCurrentDirectory() + @"\Dados.csv"; //@"Assets\Dados.csv"; //"\"C:\\Users\\Cliente\\Desktop\\Unity Projetos\\A3\\Assets\\Dados.csv\""; //@"C:\Users\Cliente\Desktop\Unity Projetos\A3\Assets\Dados.csv"; // Salvando o caminho que player salvou os dados
+
+    public static string dados_path = diretorio_padrao; 
+
+    public IEnumerator saveCSV(string linhas, bool escolherDiretorio = false)
     {
+        if (!escolherDiretorio) {
+            // Salvar o arquivo CSV
+            File.WriteAllText(diretorio_padrao, linhas);
 
-        var caminho = StandaloneFileBrowser.SaveFilePanel("Salvar arquivo CSV", "", "Dados", "csv");
+            Debug.Log("Arquivo CSV salvo em: " + diretorio_padrao);
+            //C:/Users/Cliente/AppData/LocalLow/GrupoA3/ProjetoChina
 
-        if (!string.IsNullOrEmpty(caminho))
-        {
-            // Salvar o arquivo CSV no caminho escolhido
-            File.WriteAllText(caminho, linhas);
+            yield return null;
 
-            Debug.Log("Arquivo CSV salvo em: " + caminho);
+        } else {
+
+            // Este código serve para o player escolher o local de save dos dados
+            var caminho = StandaloneFileBrowser.SaveFilePanel("Salvar arquivo CSV", "", "Dados", "csv");
+
+            if (!string.IsNullOrEmpty(caminho))
+            {
+                // Salvar o arquivo CSV no caminho escolhido
+                File.WriteAllText(caminho, linhas);
+                dados_path = caminho;
+                Debug.Log("Arquivo CSV salvo em: " + caminho);
+            }
+            else
+            {
+                Debug.Log("Nenhum caminho selecionado.");
+            }
+
         }
-        else
-        {
-            Debug.Log("Nenhum caminho selecionado.");
-        }
+
     }
 
 
